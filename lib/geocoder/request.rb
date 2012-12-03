9ee6f9dd746149989c658deaa2bc3e51a6c80e68
Chance Dinkins <chance@cyberstride.net>
2012-12-03 01:54:22 -0500

    fixing a 2am enduced dyslexic moment (172... vs 127...)

--- lib/geocoder/request.rb
+++ lib/geocoder/request.rb
@@ -5,9 +5,10 @@ module Geocoder
 
     def location
       unless defined?(@location)
-        ip = env.has_key?('HTTP_X_REAL_IP') ? env['HTTP_X_REAL_IP'].first : (env.has_key?('HTTP_X_FORWARDED_FOR') ? env['HTTP_X_FORWARDED_FOR'].first : ip)
-        ip = ip == "172.0.0.1" ? eval(open("http://ip2country.sourceforge.net/ip2c.php?format=JSON").read)[:ip] : ip
-        @location = Geocoder.search(ip).first
+        local_ip = env.has_key?('HTTP_X_REAL_IP') ? env['HTTP_X_REAL_IP'].first : (env.has_key?('HTTP_X_FORWARDED_FOR') ? env['HTTP_X_FORWARDED_FOR'].first : ip)
+        ext_ip = (local_ip == "127.0.0.1") ? eval(open("http://ip2country.sourceforge.net/ip2c.php?format=JSON").read)[:ip] : local_ip
+        puts ext_ip
+        @location = Geocoder.search(ext_ip).first
       end
       @location
     end
