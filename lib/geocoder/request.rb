b18aa7d3973c735f19e3849f671da8f5dd9af6c5
Chance Dinkins <chance@cyberstride.net>
2012-12-03 01:36:23 -0500

    cleaning up code a bit

--- lib/geocoder/request.rb
+++ lib/geocoder/request.rb
@@ -6,9 +6,7 @@ module Geocoder
     def location
       unless defined?(@location)
         ip = env.has_key?('HTTP_X_REAL_IP') ? env['HTTP_X_REAL_IP'].first : (env.has_key?('HTTP_X_FORWARDED_FOR') ? env['HTTP_X_FORWARDED_FOR'].first : ip)
-        if ip == "127.0.0.1"
-          ip = eval(open("http://ip2country.sourceforge.net/ip2c.php?format=JSON").read)[:ip]
-        end
+        ip = ip == "172.0.0.1" ? eval(open("http://ip2country.sourceforge.net/ip2c.php?format=JSON").read)[:ip] : ip
         @location = Geocoder.search(ip).first
       end
       @location
