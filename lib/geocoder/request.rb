2b138d462331072966d7cbe95a43f3a45a7e1a7f
Chance <chance@cyberstride.net>
2012-12-12 10:49:48 -0500

    Fixed issue of grabbing the first char of ip

--- lib/geocoder/request.rb
+++ lib/geocoder/request.rb
@@ -5,7 +5,7 @@ module Geocoder
 
     def location
       unless defined?(@location)
-        local_ip = env.has_key?('HTTP_X_REAL_IP') ? env['HTTP_X_REAL_IP'].first : (env.has_key?('HTTP_X_FORWARDED_FOR') ? env['HTTP_X_FORWARDED_FOR'].first : ip)
+        local_ip = env.has_key?('HTTP_X_REAL_IP') ? env['HTTP_X_REAL_IP'] : (env.has_key?('HTTP_X_FORWARDED_FOR') ? env['HTTP_X_FORWARDED_FOR'] : ip)
         ext_ip = (local_ip == "127.0.0.1") ? eval(open("http://ip2country.sourceforge.net/ip2c.php?format=JSON").read)[:ip] : local_ip
         puts ext_ip
         @location = Geocoder.search(ext_ip).first
